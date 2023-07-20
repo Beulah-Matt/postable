f8a824f51948975c5ad2f566315ad33426a0dbd4
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-20 13:21:34 +0300

    Minor style fixes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider> 
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 reportWebVitals();
