4a4ebb534bd31f6f8347ebacbac1bb2d5c4783e3
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-20 12:22:24 +0300

    ✨ App routing start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React from 'react';
+import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Feed from "./components/Feed";
+import Login from "./components/Login"
 
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={ <Root />}>
+        <Route index element={<Feed />} />
+        <Route path="/login" element={ <Login />} />
+      </Route>
+    )
+  )
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 "> 
-      <h1 className="text-3xl font-bold">Feed</h1>   
-      <Feed />
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">    
+      <RouterProvider router={router}/>
     </div>
   );
+}
+
+const Root = () => {
+  return(
+    <div>
+      <Outlet className="flex-grow" />
+    </div>
+  )
 }
 
 export default App;
