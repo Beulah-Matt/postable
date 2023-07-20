ef22140a1a312077fe6fe406b56dd4f6c242554e
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-20 13:28:56 +0300

    ✨ Navigate to userPage on login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Feed from "./components/Feed";
 import Login from "./components/Login"
+import UserPage from './components/UserPage';
 
 
 function App() {
@@ -11,11 +12,12 @@ function App() {
       <Route path="/" element={ <Root />}>
         <Route index element={<Feed />} />
         <Route path="/login" element={ <Login />} />
+        <Route path="myPage" element={ <UserPage />}/>
       </Route>
     )
   )
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 bg-blue-100">    
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 bg-blue-100 h-screen">    
       <RouterProvider router={router}/>
     </div>
   );
