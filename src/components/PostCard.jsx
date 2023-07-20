91b442c46be14e1fc8791cc8cf71af7c4c1200ad
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-20 13:59:14 +0300

    Use myPost prop to access a users posts, reuse postcard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,40 +1,46 @@
-import { useContext, useEffect, useState } from "react"
-import { AuthContext } from "../context/AuthContext"
-import postService from "../services/postsService"
-import { Link, useNavigate } from "react-router-dom"
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import postService from "../services/postsService";
+import { Link, useNavigate } from "react-router-dom";
+import userService from "../services/userService";
 
-const PostCard = () => {
-
-  const {user} = useContext(AuthContext)
-  const [posts, setPosts] = useState([])
-  const [signInPrompt, setSignInPromp] = useState(false)
+const PostCard = ({ myPosts }) => {
+  const { user } = useContext(AuthContext);
+  const [posts, setPosts] = useState([]);
+  //const [signInPrompt, setSignInPrompt] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const allPosts = await postService.getPosts();
+        let allPosts;
+        if (myPosts && user) {
+          allPosts = await postService.getMyPosts(user.id);
+        } else {
+          allPosts = await postService.getPosts();
+        }
 
-        // Associate the posts with their corresponding authors
-        const postsWithAuthors = await Promise.all(
-          allPosts.map(async (post) => {
-            const author = await postService.getAuthor(post.userId);
-            return { ...post, author };
-          })
-        );
+        // Fetch all users
+        const users = await userService.getUsers();
+
+        // Map each post to include the corresponding author
+        const postsWithAuthors = allPosts.map((post) => {
+          const author = users.find((user) => user.id === post.userId);
+          return { ...post, author };
+        });
 
         setPosts(postsWithAuthors);
       } catch (error) {
-        console.error('Error fetching posts', error);
+        console.error("Error fetching posts", error);
       }
     };
     fetchPosts();
-  }, []);
+  }, [myPosts, user]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const viewAllClick = () => {
     if (!user) {
-      navigate('/login')
+      navigate('/login');
     } else {
       // Logic to navigate to all posts page when user is signed in
       // Implement page navigation here
@@ -43,20 +49,22 @@ const PostCard = () => {
 
   return (
     <div>
-      <div className="flow-root mt-6 p-2">
-        <ul className="-my-5 divide-y divide-slate-700">
+      <div className="flow-root mt-6">
+        <ul className="-my-5 divide-y divide-gray-200">
           {posts.map((post) => (
             <li key={post.id} className="py-5">
-              <div className="relative ">
+              <div className="relative focus-within:ring-2 focus-within:ring-indigo-500">
                 <h3 className="text-sm font-semibold text-blue-800">
-                  <Link className="hover:underline focus:outline-none">
+                  <Link to="#" className="hover:underline focus:outline-none">
                     {/* Extend touch target to entire panel */}
                     <span className="absolute inset-0" aria-hidden="true" />
                     {post.title}
                   </Link>
                 </h3>
                 <p className="mt-1 text-sm text-gray-600 line-clamp-2">{post.body}</p>
-                <p className="mt-1 text-sm text-cyan-950">Author: {post.author ? post.author.name : "Unknown"}</p>
+                <p className="mt-1 text-sm text-gray-600">
+                  Author: {post.author ? post.author.name : "Unknown"}
+                </p>
               </div>
             </li>
           ))}
@@ -64,15 +72,17 @@ const PostCard = () => {
       </div>
       <div className="mt-6">
         {/* View All button */}
-        <button
-          className="w-full flex justify-center items-center px-4 py-2 mt-4 border border-slate-700 shadow-sm text-sm font-medium rounded-md text-gray-700 bg- hover:bg-gray-50"
-          onClick={viewAllClick}
-        >
-          View All
-        </button>
+        {!myPosts && (
+          <button
+            className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            onClick={viewAllClick}
+          >
+            View All
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
