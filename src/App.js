9a4bca1c46416535a77c8c5e62c62d5957fd93bd
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-20 07:49:19 +0300

    ✨ Rendering Feed page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Feed from "./components/Feed";
+
 
 function App() {
   return (
-    <div className="text-2xl">
-      Postable Posts App
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 "> 
+      <h1 className="text-3xl font-bold">Feed</h1>   
+      <Feed />
     </div>
   );
 }
