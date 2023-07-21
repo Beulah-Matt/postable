b09ff8a2bd1cdd6e72f18236a6e02f358ed466fe
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-21 19:56:24 +0300

    🎨 Add index.js file for module exports and declutter app.js

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+// index.js
+import Feed from "./Feed"
+import Login from "./Login";
+import Users from './Users';
+import Profile from './Profile';
+import AllPosts from './AllPosts';
+import MyPosts from './MyPosts';
+import Following from './Following';
+import PaymentForm from './PaymentForm';
+import UserPosts from './UserPosts';
+
+export {
+  Feed,
+  Login,
+  Users,
+  Profile,
+  AllPosts,
+  MyPosts,
+  Following,
+  PaymentForm,
+  UserPosts,
+};
