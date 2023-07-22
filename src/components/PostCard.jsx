e05aaab31aa9769cd47f6a4cba95bf9a81de653a
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-23 01:37:56 +0300

    Working on payments premium state

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useMemo } from "react";
 import { AuthContext } from "../context/AuthContext";
 import postService from "../services/postsService";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,19 +7,22 @@ import { AiOutlineLike } from "react-icons/ai";
 import PaymentForm from "./PaymentForm";
 
 const PostCard = ({ myPosts }) => {
-  const { user } = useContext(AuthContext);
-  const [posts, setPosts] = useState([]);
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const [showPaymentForm, setShowPaymentForm] = useState(false);
+  const { user, upgradeToPremium, setUser } = useContext(AuthContext);
 
-  const initialLikesData = JSON.parse(localStorage.getItem("postLikes")) || {};
-  const [postLikes, setPostLikes] = useState(initialLikesData);
-  const [likedPosts, setLikedPosts] = useState({});
-  const [postComments, setPostComments] = useState({});
-  const [showComments, setShowComments] = useState({});
+  const [state, setState] = useState({
+    posts: [],
+    showPaymentForm: false,
+    postLikes: JSON.parse(localStorage.getItem("postLikes")) || {},
+    likedPosts: {},
+    postComments: {},
+    showComments: {},
+  });
 
-  useEffect(() => {
-    const fetchPosts = async () => {
+  const navigate = useNavigate();
+
+  // Memoized fetchPosts function
+  const fetchPosts = useMemo(
+    () => async () => {
       try {
         let allPosts;
         if (myPosts && user) {
@@ -27,86 +30,115 @@ const PostCard = ({ myPosts }) => {
         } else {
           allPosts = await postService.getPosts();
         }
+
         // Fetch all users
         const users = await userService.getUsers();
+
         // Map each post to include the corresponding author
         const postsWithAuthors = allPosts.map((post) => {
           const author = users.find((user) => user.id === post.userId);
           return { ...post, author };
         });
-        setPosts(postsWithAuthors);
+
+        setState((prevState) => ({
+          ...prevState,
+          posts: postsWithAuthors,
+        }));
 
         // Fetch comments for each post and store them in the state
-        const fetchCommentsForPosts = async () => {
-          const commentsPromises = postsWithAuthors.map((post) =>
-            postService.getCommentsForPost(post.id)
-          );
-          const commentsData = await Promise.all(commentsPromises);
-
-          // Create a mapping of postId to comments
-          const commentsByPostId = {};
-          commentsData.forEach((comments, index) => {
-            commentsByPostId[postsWithAuthors[index].id] = comments;
-          });
-
-          setPostComments(commentsByPostId);
-        };
-
-        fetchCommentsForPosts();
-
+        fetchCommentsForPosts(postsWithAuthors);
       } catch (error) {
         console.error("Error fetching posts", error);
       }
-    };
+    },
+    [myPosts, user]
+  );
+
+  // Memoized fetchCommentsForPosts function
+  const fetchCommentsForPosts = useMemo(() => async (postsWithAuthors) => {
+    const commentsPromises = postsWithAuthors.map((post) =>
+      postService.getCommentsForPost(post.id)
+    );
+    const commentsData = await Promise.all(commentsPromises);
+
+    // Create a mapping of postId to comments
+    const commentsByPostId = {};
+    commentsData.forEach((comments, index) => {
+      commentsByPostId[postsWithAuthors[index].id] = comments;
+    });
+
+    setState((prevState) => ({
+      ...prevState,
+      postComments: commentsByPostId,
+    }));
+  }, []);
+
+  useEffect(() => {
     fetchPosts();
-  }, [myPosts, user]);
+  }, [fetchPosts]);
 
   useEffect(() => {
-    setIsSignedIn(user !== null);
-  }, [user]);
-
-  useEffect(() => {
-    localStorage.setItem("postLikes", JSON.stringify(postLikes));
-  }, [postLikes]);
-
-  const navigate = useNavigate();
+    localStorage.setItem("postLikes", JSON.stringify(state.postLikes));
+  }, [state.postLikes]);
 
   const viewAllClick = () => {
-    if (isSignedIn) {
-      const isSubscribed = false; // Check if the user is already subscribed or has made the payment before
+    console.log(user)
+    if (user) {
+      console.log(user);
+      const isSubscribed = user.isPremium; // Check if the user is already subscribed or has made the payment before
       if (isSubscribed) {
         navigate("/allPosts"); // If the user is subscribed, navigate to the "View All" page directly
       } else {
-        setShowPaymentForm(true); // If the user is not subscribed, show the payment form
+        setState((prevState) => ({
+          ...prevState,
+          showPaymentForm: true,
+        })); // If the user is not subscribed, show the payment form
       }
     }
   };
-
   const handlePaymentSuccess = (paymentMethodId) => {
-    // Process the payment method ID if needed (e.g., send it to the server)
-    // Then navigate to the "View All" page
     navigate("/allPosts");
+    upgradeToPremium();
+    // Update the user's premium status and save it in local storage
+    setUser((prevUser) => {
+      if (prevUser) {
+        const updatedUser = { ...prevUser, isPremium: true };
+        localStorage.setItem("user", JSON.stringify(updatedUser));
+        localStorage.setItem("isPremium", true);
+        console.log("Premium status:", updatedUser.isPremium);
+        return updatedUser;
+      }
+      return null;
+    });
   };
 
   const handleLikes = (postId) => {
-    if (!likedPosts[postId]) {
+    if (!state.likedPosts[postId]) {
       // If the post has not been liked, update the likes count and mark the post as liked
-      setPostLikes((prevLikes) => ({
-        ...prevLikes,
-        [postId]: (prevLikes[postId] || 0) + 1,
+      setState((prevState) => ({
+        ...prevState,
+        postLikes: {
+          ...prevState.postLikes,
+          [postId]: (prevState.postLikes[postId] || 0) + 1,
+        },
+        likedPosts: {
+          ...prevState.likedPosts,
+          [postId]: true,
+        },
       }));
-      // Mark the post as liked
-      setLikedPosts((prevLikedPosts) => ({
-        ...prevLikedPosts,
-        [postId]: true,
-      }));
+
+      // Update localStorage
+      localStorage.setItem("postLikes", JSON.stringify(state.postLikes));
     }
   };
-  
+
   const toggleComments = (postId) => {
-    setShowComments((prevVisibility) => ({
-      ...prevVisibility,
-      [postId]: !prevVisibility[postId],
+    setState((prevState) => ({
+      ...prevState,
+      showComments: {
+        ...prevState.showComments,
+        [postId]: !prevState.showComments[postId],
+      },
     }));
   };
 
@@ -114,7 +146,7 @@ const PostCard = ({ myPosts }) => {
     <div>
       <div className="flow-root mt-6">
         <div className="-my-5 divide-y divide-gray-900">
-          {posts.map((post) => (
+          {state.posts.map((post) => (
             <div key={post.id} className="py-5">
               <div className="relative">
                 <h3 className="text-sm font-semibold text-blue-800">
@@ -129,30 +161,38 @@ const PostCard = ({ myPosts }) => {
                   Author: {post.author ? post.author.name : "Unknown"}
                 </p>
                 <div className="flex justify-end mx-6 gap-4">
-                  <button className="flex flex-row " onClick={() => handleLikes(post.id)}>
+                  <button
+                    className="flex flex-row "
+                    onClick={() => handleLikes(post.id)}
+                  >
                     {" "}
-                    <AiOutlineLike className="h-5 w-5"/> <span className="">{postLikes[post.id] || 0} </span>
+                    <AiOutlineLike className="h-5 w-5" />{" "}
+                    <span className="">{state.postLikes[post.id] || 0} </span>
                   </button>
-    
-                   <button
+
+                  <button
                     className="text-sm text-blue-900 underline focus:outline-none"
                     onClick={() => toggleComments(post.id)}
-                    >
-                      {showComments[post.id] ? "Hide Comments" : "Show Comments"}
-                   </button>
+                  >
+                    {state.showComments[post.id]
+                      ? "Hide Comments"
+                      : "Show Comments"}
+                  </button>
                 </div>
               </div>
               <div className="mt-3 border border-s-black px-4">
-                {showComments[post.id] && postComments[post.id] && (
+                {state.showComments[post.id] && state.postComments[post.id] && (
                   <div className=" ">
-                  <h4 className="text-sm font-semibold text-gray-900">Comments:</h4>
-                  <ul className="mt-2 space-y-4">
-                    {postComments[post.id].map((comment) => (
-                      <li key={comment.id} className="text-sm text-gray-600">
-                        <strong>{comment.name}</strong> - {comment.body}
-                      </li>
-                    ))}
-                  </ul>
+                    <h4 className="text-sm font-semibold text-gray-900">
+                      Comments:
+                    </h4>
+                    <ul className="mt-2 space-y-4">
+                      {state.postComments[post.id].map((comment) => (
+                        <li key={comment.id} className="text-sm text-gray-600">
+                          <strong>{comment.name}</strong> - {comment.body}
+                        </li>
+                      ))}
+                    </ul>
                   </div>
                 )}
               </div>
@@ -160,16 +200,33 @@ const PostCard = ({ myPosts }) => {
           ))}
         </div>
       </div>
+
       <div className="mt-6">
-        {isSignedIn ? (
+        {user ? (
           <>
-            <button
-              className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-              onClick={viewAllClick}
-            >
-              View All
-            </button>
-            {showPaymentForm && <PaymentForm onPaymentSuccess={handlePaymentSuccess} />}
+            {user.isPremium ? (
+              <button
+                className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                onClick={viewAllClick}
+              >
+                View All
+              </button>
+            ) : (
+              <button
+                className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                onClick={() =>
+                  setState((prevState) => ({
+                    ...prevState,
+                    showPaymentForm: true,
+                  }))
+                }
+              >
+                View All (Premium)
+              </button>
+            )}
+            {state.showPaymentForm && (
+              <PaymentForm onPaymentSuccess={handlePaymentSuccess} />
+            )}
           </>
         ) : (
           <button
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,69 +1,93 @@
-import React, { createContext, useState, useEffect } from 'react'
-import userService from '../services/userService';
+import React, { createContext, useState, useEffect } from "react";
+import userService from "../services/userService";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    // Check if there's a user in local storage when the component mounts
+  // Check if there's a user in local storage when the component mounts
   useEffect(() => {
-    const storedUser = localStorage.getItem('user');
+    const storedUser = localStorage.getItem("user");
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      const parsedUser = JSON.parse(storedUser);
+      setUser(parsedUser);
       setLoading(false);
     } else {
       setLoading(false);
     }
   }, []);
 
-    const authenticateUser = async (username, password) => {
-        try {
-          const usersData = await userService.getUsers();
-          const authenticatedUser = usersData.find(
-            (user) => user.username === username && user.address.zipcode === password
-          );
-    
-          if (authenticatedUser) {
-            setUser(authenticatedUser);
-            // Save the authenticated user to local storage
-            localStorage.setItem('user', JSON.stringify(authenticatedUser));
-          } else {
-            setUser(null);
-          }
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching users:', error);
-          setLoading(false);
-        }
-      };
+  useEffect(() => {
+    // Check if the user is premium from local storage
+    const isPremium = localStorage.getItem("isPremium") === "true";
+    setUser((prevUser) => {
+      if (prevUser) {
+        return { ...prevUser, isPremium };
+      }
+      return null;
+    });
+  }, []);
 
-      const upgradeToPremium = () => {
-        setUser((prevUser) => ({ ...prevUser, isPremium: true }));
-      };
+  const authenticateUser = async (username, password) => {
+    try {
+      const usersData = await userService.getUsers();
+      const authenticatedUser = usersData.find(
+        (user) => user.username === username && user.address.zipcode === password
+      );
 
-      const logout = () => {
+      if (authenticatedUser) {
+        setUser(authenticatedUser);
+        // Save the authenticated user to local storage
+        localStorage.setItem("user", JSON.stringify(authenticatedUser));
+        // Set premium status in local storage
+        localStorage.setItem("isPremium", authenticatedUser.isPremium || false);
+      } else {
         setUser(null);
-        // Remove the user from local storage upon logout
-        localStorage.removeItem('user');
-      };
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      setLoading(false);
+    }
+  };
 
-      return(
-        <AuthContext.Provider
-            value={{
-            user,
-            loading,
-            authenticateUser,
-            logout,
-            // followUser,
-            // blockUser,
-            upgradeToPremium
-        }}
-        >
-          {children}  
-        </AuthContext.Provider>
-      )
-}
+  const upgradeToPremium = () => {
+    setUser((prevUser) => {
+      if (prevUser) {
+        const updatedUser = { ...prevUser, isPremium: true };
+        // Save the updated premium status to local storage
+        localStorage.setItem("user", JSON.stringify(updatedUser));
+        localStorage.setItem("isPremium", true);
+        return updatedUser;
+      }
+      return null;
+    });
+  };
 
-export {AuthContext, AuthProvider}
+  const logout = () => {
+    console.log(user, user.isPremium)
+    setUser(null);
+    // Remove the user and premium status from local storage upon logout
+    localStorage.removeItem("user");
+    localStorage.removeItem("isPremium");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        setUser,
+        authenticateUser,
+        logout,
+        upgradeToPremium,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
